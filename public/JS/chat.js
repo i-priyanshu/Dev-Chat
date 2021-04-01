21e7e407f1741b645335d82c94fb3b7ff3dee8d9
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-04-01 15:56:09 +0530

    Socket Events

--- public/JS/chat.js
+++ public/JS/chat.js
@@ -1,1 +1,16 @@
-io();
+const socket = io();
+
+socket.on("welcome", (welcome) => {
+  console.log("message => ", welcome);
+});
+
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message);
+});
--- src/index.js
+++ src/index.js
@@ -12,8 +12,13 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New websocket Connection");
+  socket.emit("welcome", "hello there!");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
 });
 
 server.listen(port, () => {
