2570f69c82143b6b49f7cffc8ae391bd3fadff66
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-04-05 17:32:17 +0530

    Bug

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -8,9 +8,9 @@ const generateMessage = (username, text) => {
 
 const generateLocationMessage = (username, url) => {
   return {
+    username,
     url,
     createdAt: new Date().getTime(),
-    username,
   };
 };
 
